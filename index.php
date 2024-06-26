def3b7b2c07c1dea6941b5ca9a9ce8429739b75c
VxneC0de <vanerubio2020@gmail.com>
2024-06-26 16:51:25 -0400

    feat: create file php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+if($_REQUEST['cod']==1){ ?>
+<h2>ARIES: Va a llover en tu camino a casa</h2>
+<?php }
+if($_REQUEST['cod']==2){ ?>
+<h2>TAURO: Vas a tener un excelente día</h2>
+<?php }
+if($_REQUEST['cod']==3){ ?>
+<h2>GEMINIS: Ganaras la loteria</h2>
+<?php }
+if($_REQUEST['cod']==4){ ?>
+<h2>CANCER: Tendras una gripe</h2>
+<?php }
+if($_REQUEST['cod']==5){ ?>
+<h2>LEO: Cuidado con tus decisiones de hoy</h2>
+<?php }
+if($_REQUEST['cod']==6){ ?>
+<h2>VIRGO: Tendras un excelente día en el trabajo</h2>
+<?php }
+if($_REQUEST['cod']==7){ ?>
+<h2>LIBRA: Problema legales</h2>
+<?php }
+if($_REQUEST['cod']==8){ ?>
+<h2>ESCORPIO: Te invita a la playa</h2>
+<?php }
+if($_REQUEST['cod']==9){ ?>
+<h2>SAGITARIO: Te cuidado donde lanzas la flecha</h2>
+<?php }
+if($_REQUEST['cod']==10){ ?>
+<h2>CAPRICORNIO: Te van a llevar al cine</h2>
+<?php }
+if($_REQUEST['cod']==11){ ?>
+<h2>ACUARIO: No es bueno que vayas a una piscina hoy</h2>
+<?php }
+if($_REQUEST['cod']==12){ ?>
+<h2>PISCIS: Tu caracter te traera problemas</h2>
+<?php }
